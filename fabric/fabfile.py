d420a2940720299939ed36e846268db969a4b88d
Andrew Miller <amiller@dappervision.com>
2011-06-28 18:37:26 -0400

    Added functions to fab file Project (tp)
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -1,6 +1,7 @@
 from fabric.api import run, sudo
 from fabric.context_managers import cd
 import time
+import os
 
 
 def install_picarus():
@@ -15,5 +16,32 @@ def install_picarus():
         run('tar -xjf OpenCV-2.2.0.tar.bz2')
 
 
+def install_git(repo):
+    run('git clone %s' % repo)
+    with cd(os.path.basename(repo)):
+        run('python setup.py build')
+        sudo('python setup.py install')
+
+
 def install_dickarus():  # Andrew's fucntion
-    pass
+    run('uname -s')
+    work_dir = 'picarus-%f' % time.time()
+    run('mkdir %s' % work_dir)
+    with cd(work_dir):
+
+        # Apt Get
+        sudo('apt-get install -y ffmpeg libavcodec-dev libswscale-dev libavformat-dev fftw3-dev python-dev build-essential python-imaging python-numpy python-scipy git-core python-setuptools')
+
+        # Setuptools
+        sudo('easy_install cython numpy PIL scons')
+
+        # Pyffmpeg, keyframe, vidfeat
+        install_git('https://github.com/amiller/pyffmpeg')
+        install_git('https://github.com/bwhite/vidfeat')
+        install_git('https://github.com/bwhite/imfeat')
+        install_git('https://github.com/bwhite/keyframe')
+        install_git('https://github.com/bwhite/classipy')
+        install_git('https://github.com/bwhite/hadoopy')
+        install_git('https://github.com/bwhite/impoint')
+        install_git('https://github.com/bwhite/hadoopy_flow')
+        install_git('https://github.com/bwhite/picarus')
